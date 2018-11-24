766873c8130a012087ce0180769170bd35a46854
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-24 16:25:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ function destructivelyAppendKitten(name) {
 
 
 //*************************************************
-function destructivelyPrependKitten (name)
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+  return kittens;
+}
